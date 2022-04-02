0c8f716d5090568a1aa22368680db3399ecc1bae
Christophe Pelletier <christophe.pelletier.contact@gmail.com>
2022-04-02 18:29:37 +0200

    save

--- front/src/views/SignupView.vue
+++ front/src/views/SignupView.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="about">
-      <img alt="Vue logo" src="../assets/logo.png">
+    <img alt="Vue logo" src="../assets/logo.png" />
     <h1>Signup</h1>
-    <input type="email" name="email" placeholder= "email"/>
-    <input type="text" name="nom" placeholder= "nom"/>
+    <div>
+      <input type="email" name="email" placeholder="email" />
+    </div>
+    <input type="text" name="nom" placeholder="nom" />
+    <button>S'inscrire</button>
   </div>
 </template>
 
