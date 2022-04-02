9f8d21d9d132eaf83c8cf9ec69121c3dcfb87dd6
Christophe Pelletier <christophe.pelletier.contact@gmail.com>
2022-04-02 18:52:02 +0200

    bouton signup réagit

--- front/src/views/SignupView.vue
+++ front/src/views/SignupView.vue
@@ -3,10 +3,30 @@
     <img alt="Vue logo" src="../assets/logo.png" />
     <h1>Signup</h1>
     <div>
-      <input type="email" name="email" placeholder="email" />
+      <input type="email" name="email" v-model="email" placeholder="email" />
     </div>
-    <input type="text" name="nom" placeholder="nom" />
-    <button>S'inscrire</button>
+    <input type="text" name="nom" v-model="name" placeholder="nom" />
+    <button @click="signup">S'inscrire</button>
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      email: "",
+      name: "",
+    };
+  },
+  methods: {
+    signup() {
+      console.log("click register button");
+    },
+  },
+  watch: {
+    email(value) {
+      console.log("email : ", value);
+    },
+  },
+};
+</script>
