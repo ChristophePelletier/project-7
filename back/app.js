df76464b1df45a0c2d3aa1c85876b62fa104cf22
Christophe Pelletier <christophe.pelletier.contact@gmail.com>
2022-04-02 11:16:14 +0200

    test serveur ok

--- back/app.js
+++ back/app.js
@@ -1,1 +1,23 @@
-console.log("test")
+const cors = require('cors')
+const express = require('express')
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.get('/test', (req, res) => {
+	console.log('test')
+	res.send({
+		message: 'test',
+	})
+})
+
+app.post('/signup', (req, res) => {
+	res.send({
+		message: `Bonjour ${req.body.email} utilisateur enregistré`,
+	})
+})
+
+app.listen(process.env.PORT || 3000)
+
+
