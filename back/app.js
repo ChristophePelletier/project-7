6d6795610be7b64be33c2572dcd66e9678510e1f
Christophe Pelletier <christophe.pelletier.contact@gmail.com>
2022-04-05 07:03:37 +0200

    save

--- back/app.js
+++ back/app.js
@@ -2,7 +2,7 @@ const cors = require('cors')
 const express = require('express')
 
 const app = express()
-const Sequelize = require('sequelize')
+
 app.use(cors())
 app.use(express.json())
 
@@ -11,6 +11,7 @@ const PORT = process.env.PORT || 3000
 
 db.sequelize
   .sync()
+  .then(() => console.log('OK Database connected OK'))
   .then(() => {
     app.listen(PORT)
     console.log(`Serveur en fonction sur le port : ${PORT}.`)
@@ -59,4 +60,43 @@ app.get('/test', (req, res) => {
 
 /*
 //app.listen(process.env.PORT || 3000)
+*/
+
+//v1
+/*
+const cors = require('cors')
+const express = require('express')
+
+const app = express()
+const { Sequelize } = require('sequelize')
+app.use(cors())
+app.use(express.json())
+app.get('/test', (req, res) => {
+  console.log('test')
+  res.send({
+    message: 'test',
+  })
+})
+
+//Data base connection
+//require('./database/connection')
+
+const sequelize = new Sequelize('test', 'Christophe_Pelletier', '87420_Mysql', {
+  host: 'localhost',
+  dialect: 'mysql',
+})
+
+sequelize
+  .authenticate()
+  .then(() => console.log('OK Database connected OK'))
+  .catch((err) => console.log('Error: ' + err))
+
+app.post('/signup', (req, res) => {
+  console.log('req.body : ', req.body)
+  res.send({
+    message: `Bonjour bonjour ${req.body.name}, ${req.body.email} utilisateur enregistré, votre mot de passe : ${req.body.password}`,
+  })
+})
+
+app.listen(process.env.PORT || 3000)
 */
