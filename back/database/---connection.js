fb41de95843b06b2bfdf7d0d96d9361ccd9f168e
Christophe Pelletier <christophe.pelletier.contact@gmail.com>
2022-04-05 10:29:24 +0200

    save

--- back/database/---connection.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require('sequelize')
-
-const sequelize = new Sequelize('test', 'Christophe_Pelletier', '87420_Mysql', {
-  host: 'localhost',
-  dialect: 'mysql',
-})
-
-module.exports = sequelize
-global.sequelize = sequelize
