25cb0605436f119a02bc05f9f414532865559a45
Christophe Pelletier <christophe.pelletier.contact@gmail.com>
2022-04-07 15:50:24 +0200

    joi

--- back/middlewares/authPolicy.js
+++ back/middlewares/authPolicy.js
@@ -3,7 +3,7 @@ const Joi = require('joi')
 module.exports = (req, res, next) => {
   const schema = Joi.object({
     email: Joi.string().email(),
-    password: Joi.string().regex(new RegExp('^[a-zA-Z0-9]{8,32}$')),
+    password: Joi.string().regex(new RegExp('^[a-zA-Z0-9]{5,12}$')),
   })
 
   const { error } = schema.validate(req.body)
@@ -12,17 +12,12 @@ module.exports = (req, res, next) => {
     switch (error.details[0].context.key) {
       case 'email':
         res.status(400).send({
-          error: 'You must provide a valid email address',
+          error: 'Entrer une adresse mail valide',
         })
         break
       case 'password':
         res.status(400).send({
-          error: `The password provided failed to match the following rules:
-              <br>
-              1. It must contain ONLY the following characters: lower case, upper case, numerics.
-              <br>
-              2. It must be at least 8 characters in length and not greater than 32 characters in length.
-            `,
+          error: `Le mot de passe ne rentre pas dans nos règles de sécurité informatique.<br/> 1 Le mot de passe doit contenir uniquement minuscules, majuscules et nombres. 2 entre 5 et 12 caractères`,
         })
         break
       default:
