975957807e3fb8e4f5b27dcf27dc801a75bd35ea
Christophe Pelletier <christophe.pelletier.contact@gmail.com>
2022-04-10 15:15:56 +0200

    ajout bearer token dans le header après login

--- front/src/services/api.js
+++ front/src/services/api.js
@@ -1,13 +1,11 @@
 // axios object that points to the backend
 
 import axios from "axios";
-
+import store from "@/store/index.js";
 export default () =>
   axios.create({
     baseURL: "http://localhost:3000",
-    /*
     headers: {
       Authorization: `Bearer ${store.state.token}`,
     },
-    */
   });
