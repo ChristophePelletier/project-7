9adc3a0f3f95d83e0d20a6a493dd43b48e36a55c
Christophe Pelletier <christophe.pelletier.contact@gmail.com>
2022-04-20 15:24:47 +0200

    save

--- back/controllers/comments.js
+++ back/controllers/comments.js
@@ -17,48 +17,6 @@ const createComment = async (req, res) => {
   const comment = await Comment.create(data)
   res.status(200).send(comment)
 }
-
 module.exports = {
   createComment,
 }
-
-/*
-const getComments = async (req, res) => {
-  console.log('getComments')
-  console.log(req)
-  const artId = req.body.articleId
-  console.log(artId)
-  Comment.findAll()
-    .then((comment) => {
-      console.log(comment)
-      res.status(200).json(comment)
-    })
-    .catch((error) => {
-      res.status(404).json({
-        error: error,
-      })
-    })
-}
-module.exports = {
-  createComment,
-  getComments,
-}
-
-
-const getComments = async (req, res) => {
-  console.log('getComments')
-  console.log(req)
-  const artId = req.body.articleId
-  console.log(artId)
-  Comment.findAll({ where: { articleId: artId } })
-    .then((comment) => {
-      console.log(comment)
-      res.status(200).json(comment)
-    })
-    .catch((error) => {
-      res.status(404).json({
-        error: error,
-      })
-    })
-}
-*/
