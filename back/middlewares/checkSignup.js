0eaffa168292756651828e9a8d774730b57a1e4e
Christophe Pelletier <christophe.pelletier.contact@gmail.com>
2022-04-24 18:14:32 +0200

    save correction middleware

--- back/middlewares/checkSignup.js
+++ back/middlewares/checkSignup.js
@@ -2,10 +2,10 @@ const db = require('../models')
 const User = db.user
 
 module.exports = (req, res, next) => {
-  if (!req.body.firstName || !req.body.secondName)
+  if (!req.body.firstName || !req.body.secondName) {
     console.log('formulaire non comlet')
-  res.status(400).send({
-    error: `<p>Erreur : remplir nom et prénom</p>`,
-  })
-  return
+    res.status(400).send({
+      error: `<p>Erreur : remplir nom et prénom</p>`,
+    })
+  } else next()
 }
