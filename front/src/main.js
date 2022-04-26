e763dde93f70fd0827311e10fb6b8e369ae142f7
Christophe Pelletier <christophe.pelletier.contact@gmail.com>
2022-04-26 19:13:48 +0200

    save

--- front/src/main.js
+++ front/src/main.js
@@ -1,9 +1,9 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 import "./assets/global.css";
 //import "./assets/custom.scss";
