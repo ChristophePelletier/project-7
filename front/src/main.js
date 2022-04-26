4a672e7f35da865ba3c948532a1c764f2ce609cf
Christophe Pelletier <christophe.pelletier.contact@gmail.com>
2022-04-26 19:01:58 +0200

    save

--- front/src/main.js
+++ front/src/main.js
@@ -2,8 +2,9 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import "./assets/global.css";
-//import "./assets/custom.scss";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
+import "./assets/global.css";
+//import "./assets/custom.scss";
 createApp(App).use(store).use(router).mount("#app");
