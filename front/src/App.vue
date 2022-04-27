cd259b6ac9cad3e51480a975af2b867763dd4f57
Christophe Pelletier <christophe.pelletier.contact@gmail.com>
2022-04-27 18:37:25 +0200

    save

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,47 +1,54 @@
 <template>
   <header>
+    <!--MENU-->
     <nav>
       <div>
-        <router-link to="/">GROUPOMANIA</router-link>
-        <div>
-          <div>
-            <router-link to="/" class="nav-link active">Accueil</router-link>
-            <router-link
-              v-if="$store.state.user.userId !== -1"
-              to="/article-create"
-              class="nav-link active"
-            >
-              Rédiger un article
-            </router-link>
-            <router-link
-              v-if="$store.state.user.userId == -1"
-              to="/login"
-              class="nav-link active"
-            >
-              Login
-            </router-link>
-            <a
-              v-if="$store.state.user.userId !== -1"
-              v-on:click="logout()"
-              class="nav-link active pointer"
-            >
-              Déconnexion
-            </a>
-            <a
-              v-if="$store.state.user.userId !== -1"
-              v-on:click="monCompte()"
-              class="nav-link active pointer"
-            >
-              Mon compte
-            </a>
-            <!-- v-if="$store.state.user.admin == true" -->
-            <router-link to="/admin" class="nav-link active">
-              Administration
-            </router-link>
-          </div>
-        </div>
+        <router-link to="/" class="nav-link active">Accueil</router-link>
+      </div>
+      <div>
+        <router-link
+          v-if="$store.state.user.userId !== -1"
+          to="/article-create"
+          class="nav-link active"
+        >
+          Rédiger un article
+        </router-link>
+      </div>
+      <div>
+        <router-link
+          v-if="$store.state.user.userId == -1"
+          to="/login"
+          class="nav-link active"
+        >
+          Login
+        </router-link>
+      </div>
+      <div>
+        <a
+          v-if="$store.state.user.userId !== -1"
+          v-on:click="logout()"
+          class="nav-link active pointer"
+        >
+          Déconnexion
+        </a>
+      </div>
+      <div>
+        <a
+          v-if="$store.state.user.userId !== -1"
+          v-on:click="monCompte()"
+          class="nav-link active pointer"
+        >
+          Mon compte
+        </a>
+      </div>
+      <div>
+        <!-- v-if="$store.state.user.admin == true" -->
+        <router-link to="/admin" class="nav-link active">
+          Administration
+        </router-link>
       </div>
     </nav>
+    <!--FIN DU MENU-->
   </header>
   <router-view />
   <foot />
