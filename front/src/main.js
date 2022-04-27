b352edc9532ddf01b448f90a2c8d9dbf658c2622
Christophe Pelletier <christophe.pelletier.contact@gmail.com>
2022-04-27 14:41:19 +0200

    save

--- front/src/main.js
+++ front/src/main.js
@@ -3,12 +3,12 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./assets/global.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap";
+//import "bootstrap/dist/css/bootstrap.min.css";
+//import "bootstrap";
 createApp(App).use(store).use(router).mount("#app");
 
 //import "./assets/custom.scss";
-import "./styles/custom-bootstrap.scss";
+//import "./styles/custom-bootstrap.scss";
 
 /*
 https://insidert.medium.com/customizing-bootstrap-inside-vue-cli-project-c2e756c9fa29
