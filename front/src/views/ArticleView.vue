9e37719bfae23bc5202c60799cf7007aed8ac630
Christophe Pelletier <christophe.pelletier.contact@gmail.com>
2022-04-29 14:49:27 +0200

    save

--- front/src/views/ArticleView.vue
+++ front/src/views/ArticleView.vue
@@ -2,90 +2,94 @@
   <article id="one-item">
     <!--article -->
     <div id="one-article">
-      <h1>
-        {{ article.title }}
-      </h1>
-      <p class="auteur">
-        <router-link :to="'/user/' + article.userId">
-          <span class="coldInfo">par</span>
-          {{ article.firstName }} {{ article.secondName }} <br /><span
-            class="coldInfo"
-          >
-            {{ article.email }}</span
-          ></router-link
-        >
-      </p>
-      <img
-        v-if="article.image"
-        class="illustration"
-        v-bind:src="article.image"
-      />
-      <p class="date">
-        Article rédigé le :
-        {{ getFormattedDate(article.createdAt) }}
-      </p>
-      <p class="article-content">
-        {{ article.content }}
-      </p>
-      <p>
-        Article rédigé le :
-        {{ getFormattedDate(article.createdAt) }}
-      </p>
       <div>
-        <!--v-if="$store.state.user.admin == true" -->
-        <button
-          type="button"
-          class="alert"
-          @click="deleteOneArticle(article.id)"
-        >
-          <span> Supprimer cet article</span>
-        </button>
-        <p>
-          Attention, la suppression de l'article entraînera également la
-          suppression de l'ensemble des commentaires associés.
-        </p>
-      </div>
-      <!--fin de l'article -->
-      <!--commenter l'article -->
-      <div>
-        <button type="button" @click="showForm = !showForm">
-          <span> Commenter l'article</span>
-        </button>
-        <form class="commentaire" v-show="showForm">
-          <div>
-            <input
-              class="form-control"
-              id="titleComment"
-              type="text"
-              name="title"
-              v-model="comment.title"
-              placeholder="titre"
-            />
-          </div>
+        <div>
+          <h1>
+            {{ article.title }}
+          </h1>
+          <p class="article-content">
+            {{ article.content }}
+          </p>
+        </div>
+        <div>
+          <p>
+            Article rédigé le :
+            {{ getFormattedDate(article.createdAt) }}
+          </p>
+          <p class="auteur">
+            <router-link :to="'/user/' + article.userId">
+              <span class="coldInfo">par</span>
+              {{ article.firstName }} {{ article.secondName }} <br /><span
+                class="coldInfo"
+              >
+                {{ article.email }}</span
+              ></router-link
+            >
+          </p>
+          <img
+            v-if="article.image"
+            class="illustration"
+            v-bind:src="article.image"
+          />
+        </div>
+        <div>
+          <button type="button" @click="showForm = !showForm">
+            <span> Commenter l'article</span>
+          </button>
+          <form class="commentaire" v-show="showForm">
+            <div>
+              <input
+                class="form-control"
+                id="titleComment"
+                type="text"
+                name="title"
+                v-model="comment.title"
+                placeholder="titre"
+              />
+            </div>
 
-          <div>
-            <textarea
-              class="form-control"
-              type="textarea"
-              name="content"
-              v-model="comment.content"
-              placeholder="mon commentaire"
-              rows="155"
-              maxlength="8000"
-            />
-          </div>
-          <div>
-            <button type="button" class="btn btn-primary" @click="create">
-              <span>Envoyer mon commentaire</span>
-            </button>
-          </div>
-        </form>
+            <div>
+              <textarea
+                class="form-control"
+                type="textarea"
+                name="content"
+                v-model="comment.content"
+                placeholder="mon commentaire"
+                rows="155"
+                maxlength="8000"
+              />
+            </div>
+            <div>
+              <button type="button" class="btn btn-primary" @click="create">
+                <span>Envoyer mon commentaire</span>
+              </button>
+            </div>
+          </form>
+        </div>
+        <div>
+          <!--v-if="$store.state.user.admin == true" -->
+          <button
+            type="button"
+            class="alert"
+            @click="deleteOneArticle(article.id)"
+          >
+            <span> Supprimer cet article</span>
+          </button>
+          <p>
+            Attention, la suppression de l'article entraînera également la
+            suppression de l'ensemble des commentaires associés.
+          </p>
+        </div>
       </div>
-      <!--fin commenter l'article -->
     </div>
+    <!--fin de l'article -->
+    <!--commenter l'article -->
+
+    <!--fin commenter l'article -->
+
     <div id="all-articles">
       <!--commentaires -->
-      <h2>Tous les commentaires</h2>
+      <h2>Commentaires</h2>
       <article class="comment-one" v-for="comment in comments" :key="comment">
         <p>
           Titre<br />
@@ -197,6 +201,6 @@ export default {
 
 <style scoped>
 img.illustration {
-  width: 250px;
+  max-width: 200px;
 }
 </style>
